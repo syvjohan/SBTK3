f1b4da812c22efa62f3108aa01e5026946fb9777
Johan Fredriksson <johanfredriksson1987@gmail.com>
2013-05-23 16:54:26 +0200

    A new and stable version of SBTK2.
    Signed-off-by: Johan Fredriksson <johanfredriksson1987@gmail.com>

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.previewPicBox = new System.Windows.Forms.PictureBox();
+            this.composePnl = new System.Windows.Forms.Panel();
+            this.loadImgDialog = new System.Windows.Forms.OpenFileDialog();
+            this.imagePnl = new System.Windows.Forms.FlowLayoutPanel();
+            this.previewPnl = new System.Windows.Forms.Panel();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewPicBox)).BeginInit();
+            this.previewPnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(890, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.loadImageToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // previewPicBox
+            // 
+            this.previewPicBox.BackColor = System.Drawing.SystemColors.Control;
+            this.previewPicBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.previewPicBox.Location = new System.Drawing.Point(0, 0);
+            this.previewPicBox.Name = "previewPicBox";
+            this.previewPicBox.Size = new System.Drawing.Size(433, 55);
+            this.previewPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.previewPicBox.TabIndex = 3;
+            this.previewPicBox.TabStop = false;
+            // 
+            // composePnl
+            // 
+            this.composePnl.AutoScroll = true;
+            this.composePnl.BackColor = System.Drawing.Color.White;
+            this.composePnl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.composePnl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.composePnl.Location = new System.Drawing.Point(492, 24);
+            this.composePnl.Name = "composePnl";
+            this.composePnl.Size = new System.Drawing.Size(398, 492);
+            this.composePnl.TabIndex = 4;
+            // 
+            // loadImgDialog
+            // 
+            this.loadImgDialog.FileName = "openFileDialog1";
+            // 
+            // imagePnl
+            // 
+            this.imagePnl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imagePnl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.imagePnl.Location = new System.Drawing.Point(0, 24);
+            this.imagePnl.Name = "imagePnl";
+            this.imagePnl.Size = new System.Drawing.Size(200, 492);
+            this.imagePnl.TabIndex = 5;
+            // 
+            // previewPnl
+            // 
+            this.previewPnl.AutoScroll = true;
+            this.previewPnl.BackColor = System.Drawing.SystemColors.Control;
+            this.previewPnl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.previewPnl.Controls.Add(this.previewPicBox);
+            this.previewPnl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.previewPnl.Location = new System.Drawing.Point(200, 24);
+            this.previewPnl.Name = "previewPnl";
+            this.previewPnl.Size = new System.Drawing.Size(292, 492);
+            this.previewPnl.TabIndex = 0;
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToolStripMenuItem.Text = "Export...";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearToolStripMenuItem.Text = "New";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 516);
+            this.Controls.Add(this.composePnl);
+            this.Controls.Add(this.previewPnl);
+            this.Controls.Add(this.imagePnl);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewPicBox)).EndInit();
+            this.previewPnl.ResumeLayout(false);
+            this.previewPnl.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox previewPicBox;
+        private System.Windows.Forms.Panel composePnl;
+        private System.Windows.Forms.OpenFileDialog loadImgDialog;
+        private System.Windows.Forms.FlowLayoutPanel imagePnl;
+        private System.Windows.Forms.Panel previewPnl;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> clips;
+        bool isCreatingRect = false;
+        Rectangle currentClipRect;
+        Point beginRectPt;
+        Point endRectPt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            previewPicBox.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            currentClipRect = Rectangle.Empty;
+            clips = new List<PictureBox>();
+
+            loadImgDialog.Filter = "*.png, *.jpg, *.bmp|*.png;*.jpg;*.bmp";
+
+            SetupEventHandlers();
+        }
+
+        void SetupEventHandlers()
+        {
+            // Eventhandler to begin/end creating a rectangle for clips.
+            previewPicBox.Click += delegate(object sender, EventArgs e)
+            {
+                isCreatingRect = !isCreatingRect;
+                if (isCreatingRect)
+                {
+                    beginRectPt = previewPicBox.PointToClient(new Point(MousePosition.X, MousePosition.Y));
+                }                
+            };
+
+            // Eventhandler to resize rectangle.
+            previewPicBox.MouseMove += delegate(object sender, MouseEventArgs ma)
+            {
+                if (isCreatingRect)
+                {
+                    endRectPt = new Point(ma.X, ma.Y);
+                    previewPicBox.Invalidate(Rectangle.Empty, false);
+                }
+            };
+
+            previewPicBox.Paint += delegate(object sender, PaintEventArgs pea)
+            {
+                currentClipRect = RectFromMinMax(beginRectPt, endRectPt);
+                if (currentClipRect != Rectangle.Empty && previewPicBox.Image != null)
+                {
+                    Graphics g = pea.Graphics;
+                    g.DrawRectangle(Pens.White, currentClipRect);
+                }
+            };
+
+            composePnl.Click += delegate(object sender, EventArgs ea)
+            {
+                if (currentClipRect != Rectangle.Empty)
+                {
+                    PictureBox pb = GetPreviewImageSnapshot();
+
+                    bool isSelected = false;
+                    bool isDragging = false;
+                    Point offset = Point.Empty;
+
+                    // Lets us start dragging the control around, and sends all others to the back.
+                    pb.MouseDown += delegate(object s, MouseEventArgs ma)
+                    {
+                        isDragging = true;
+                        offset = ma.Location;
+
+                        pb.BringToFront();
+                        pb.Focus();
+                    };
+
+                    pb.GotFocus += delegate(object s, EventArgs e) 
+                    { 
+                        isSelected = true;
+                        pb.Invalidate();
+                    };
+                    pb.LostFocus += delegate(object s, EventArgs e) {
+                        isSelected = false;
+                        pb.Invalidate();
+                    };
+
+                    pb.Paint += delegate(object s, PaintEventArgs e)
+                    {
+                        if (isSelected)
+                        {
+                            e.Graphics.DrawRectangle(
+                                Pens.Gold,
+                                new Rectangle(
+                                    pb.ClientRectangle.X,
+                                    pb.ClientRectangle.Y,
+                                    pb.ClientRectangle.Width - 1,
+                                    pb.ClientRectangle.Height - 1
+                                )
+                            );
+                        }
+                    };
+
+                    // Stop dragging.
+                    pb.MouseUp += delegate(object s, MouseEventArgs ma)
+                    {
+                        isDragging = false;
+                        pb.Invalidate();
+                    };
+
+                    // Lets us move the picturebox into its current location.
+                    pb.MouseMove += delegate(object s, MouseEventArgs ma)
+                    {
+                        if (isDragging)
+                        {
+                            Point pos = composePnl.PointToClient(pb.PointToScreen(ma.Location));
+
+                            pb.Location = new Point(pos.X - offset.X, pos.Y - offset.Y);
+                        }
+                    };
+
+                    pb.PreviewKeyDown += delegate(object s, PreviewKeyDownEventArgs ke)
+                    {
+                        if (ke.KeyCode == Keys.Delete)
+                        {
+                            clips.Remove(pb);
+                            composePnl.Controls.Remove(pb);
+                        }
+                    };
+
+                    // Add clips to our clip list and the controls of the composePnl.
+                    clips.Add(pb);
+                    composePnl.Controls.Add(pb);
+                    currentClipRect = Rectangle.Empty;
+                    beginRectPt = endRectPt = Point.Empty;
+                }
+
+            };
+        }
+
+        PictureBox GetPreviewImageSnapshot()
+        {
+            Size imageSize = new Size(currentClipRect.Width, currentClipRect.Height);
+
+            Image img = new Bitmap(imageSize.Width, imageSize.Height);
+
+            Graphics g = Graphics.FromImage(img);
+
+            g.DrawImage(
+                previewPicBox.Image,
+                new Rectangle(0, 0, imageSize.Width, imageSize.Height),
+                currentClipRect,
+                GraphicsUnit.Pixel
+            );
+
+            PictureBox pb = new PictureBox();
+            pb.Image = img;
+            pb.MaximumSize = pb.MinimumSize = imageSize;
+
+            return pb;
+        }
+
+        // FACTORY METHOD OF PURE AWESOMENESS? ???? ?? ? 
+        Rectangle RectFromMinMax(Point a, Point b)
+        {
+            Rectangle rect = Rectangle.Empty;
+
+            // Configure X and width.
+            if (a.X > b.X)
+            {
+                rect.X = b.X;
+                rect.Width = a.X - b.X;
+            }
+            else
+            {
+                rect.X = a.X;
+                rect.Width = b.X - a.X;
+            }
+
+            // Configure Y and height.
+            if (a.Y > b.Y)
+            {
+                rect.Y = b.Y;
+                rect.Height = a.Y - b.Y;
+            }
+            else
+            {
+                rect.Y = a.Y;
+                rect.Height = b.Y - a.Y;
+            }
+
+            
+            return rect;
+        }
+
+        // Exports an image from our clips.
+        void ExportImage(string filename)
+        {
+            // Will hold image size.
+            Size sz = Size.Empty;
+
+            foreach (PictureBox pb in clips)
+            {
+                int offsetX = pb.Location.X + pb.Width;
+                int offsetY = pb.Location.Y + pb.Height;
+
+                sz.Width = (offsetX > sz.Width) ? offsetX : sz.Width;
+                sz.Height = (offsetY > sz.Height) ? offsetY : sz.Height;
+            }
+            
+
+            Image bmp = new Bitmap(sz.Width, sz.Height);
+
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.FillRectangle(Brushes.Transparent, new Rectangle(0, 0, bmp.Width, bmp.Height));
+
+            foreach (PictureBox pb in clips)
+            {
+                g.DrawImage(pb.Image, pb.Location);
+            }
+
+            using (FileStream stream = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                bmp.Save(stream, ImageFormat.Png);
+            };
+        }
+
+        #region EVENTHANDLERS
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = loadImgDialog.ShowDialog();
+
+            if (dr != DialogResult.OK)
+                return;
+
+
+            Image img = Image.FromFile(loadImgDialog.FileName);
+
+            PictureBox box = new PictureBox();
+            box.Image = img;
+            box.MaximumSize = new Size(32, 32);
+            box.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            box.Click += delegate (object s, EventArgs ea)
+            {
+                previewPicBox.Image = box.Image;
+            };
+
+            imagePnl.Controls.Add(box);
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "*.png|*.png";            
+
+            DialogResult dr = sfd.ShowDialog();
+
+            if (dr != DialogResult.OK)
+            {
+                return;
+            }
+
+            
+            ExportImage(sfd.FileName.EndsWith(".png") ? sfd.FileName : sfd.FileName + ".png");
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clips.Clear();
+            composePnl.Controls.Clear();
+        }
+
+        #endregion
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Press delete to remove images lol.");
+        }
+
+
+    }
+
+    
+}
